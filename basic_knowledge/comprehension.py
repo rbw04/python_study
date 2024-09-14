5241406ba0ebfb00376b1d64382986e2bc03b15c
rbw04 <superdino1234@gmail.com>
2024-09-14 10:57:18 +0900

    반복문~람다함수

---
+++ basic_knowledge/comprehension.py
@@ -0,0 +1,9 @@
+#for i in range(5):
+#    print("ver",i)
+#odd=[k for k in range(6) if k%2==1]
+#print(odd)
+#pairs=[(a,b) for a in range(5) for b in range(4) if a>=b]
+#print(pairs)
+#string='dinosaurs'
+#letter_count={letter:string.count(letter) for letter in set(string)}
+#print(letter_count)
--- basic_knowledge/iteration.py
+++ basic_knowledge/iteration.py
@@ -1,5 +1,4 @@
-#for u in range(5):
-#    print(u)
+#for u in range(5):#    print(u)
 #point=3
 #while point<15 :
 #    print(point)
@@ -8,16 +7,3 @@
 #    if k==4:
 #        continue
 #    print(k)
-#for c in range(10):
-#    if c==8:
-#        break
-#    print(c)
-#for i in range(5):
-#    print("ver",i)
-#odd=[k for k in range(6) if k%2==1]
-#print(odd)
-#pairs=[(a,b) for a in range(5) for b in range(4) if a>=b]
-#print(pairs)
-#string='dinosaurs'
-#letter_count={letter:string.count(letter) for letter in set(string)}
-#print(letter_count)
